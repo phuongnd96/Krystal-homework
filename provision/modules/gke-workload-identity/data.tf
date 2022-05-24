048a09d751b80ae499d178d2586e213765921959
duyphuong <you@example.com>
2022-05-25 01:47:53 +0700

    refactor: remove unused module

--- provision/modules/gke-workload-identity/data.tf
+++
@@ -1,7 +0,0 @@
-data "google_client_config" "default" {}
-
-data "google_container_cluster" "my_cluster" {
-  project = var.project_id
-  name     = var.cluster_name
-  location = var.cluster_zone
-}
